c342b4dcd9717aaab8d4338398de8af3829c8c95
manos <manos.zalokostas@gmail.com>
2020-05-05 02:05:21 +0300

    Populate the "links" section instaed of 1 link allowed before

---
+++ intro.js
@@ -0,0 +1,305 @@
+export const Intro = () => {
+
+    return (
+        `
+        <div id="introduction">
+    <div id="introduction_menu">
+    <ul>
+    <li><a id="introduction_cv" class='selected' href="#">Welcome</a></li>
+<li><a id="resume_cv" href="#">Timeline</a></li>
+<li><a id="interactive_cv" href="#">Interactive</a></li>
+<li><a id="document_cv" href="#">Document</a></li>
+</ul>
+</div>
+<h1>Resume</h1>
+<div id="skills_preview" class="monitor">
+    <div id='skill_fields' class='mbuttons'><b title='project'>projects</b><b title="reference">references</b><b title="tool">skills</b><em>||</em></div>
+<div id="welcome">
+    <h3>Web Development</h3>
+<h3>Web Design</h3>
+<h3>Graphics Design</h3>
+<h3 style="color:goldenrod">Welcome</h3>
+    </div>
+
+    <ul>
+    <li id='project' class='slide_animated'>
+    <div class='mitem'>
+    <h2>Projects</h2>
+    <h3>Project Name</h3>
+<h4>Tech Field</h4>
+<p>Description</p>
+<img/>
+</div>
+<div>&nbsp;</div>
+</li>
+<li id='reference'>
+    <div class='mitem'>
+    <h2>References</h2>
+    <h3>Instructor</h3>
+    <h4>Instructor Title</h4>
+<p>Comment</p>
+<img/>
+</div>
+<div>&nbsp;</div>
+</li>
+<li id="tool">
+    <div class='mitem'>
+    <h2>Skills</h2>
+    <div class='mgroupi'>
+    <h4>Tools</h4>
+    </div>
+    <div class="mgroupi">
+    <h4>Tools</h4>
+    </div>
+    <div class='mgroupi'>
+    <h4>Tools</h4>
+    </div>
+    <div class='mgroupi'>
+    <h4>Tools</h4>
+    </div>
+    <div class='mgroupi'>
+    <h4>Tools</h4>
+    </div>
+    <div class='mgroupi'>
+    <h4>Tools</h4>
+    </div>
+    <div class='mgroupi'>
+    <h4>Tools</h4>
+    </div>
+    <div class='mgroupi'>
+    <h4>Tools</h4>
+    </div>
+    </div>
+    <div>&nbsp;</div>
+</li>
+</ul>
+</div>
+<div id="author_presentation">
+    <p>Creativity is the tool that while enjoy using and practicing is leading me a nice journey throughout diverse educational and occupational experiences.
+    Unfolding my need for being creative and enthousiasm for technology I started my training on graphics design. Leveraging my skills on the
+area have let me acknowledge spare concepts around web technology. The need for obtaining fundamental understanding has gradually overwhelmed me, and drove me to migrate interest on the field. <br/>
+</p>
+<img src="images/profile/manos2.jpg"/>
+    </div>
+    <div id="interactive_intro" class='citation'>
+    <div id="resume_cv_field">
+    <div class="aux_txt">
+    <h3>Timeline <span>comments<input type="checkbox" id="cv_comments"/></span></h3>
+<p id="cv_description">
+    <i>After concluding studying at official domestic 12 year education and army obligations for the country, I started to inquiry knowledge that would ideally fit properly to my potentials.
+    Driven by wide require for creativity, inclination on graphic design, and regarded technologies gaining high popularity back on 2000 I have decided to continue my education.
+<br/><br/></i>
+
+
+<strong> In 2017</strong> Contracted as freelance, standalone Full stack Developer to design and build a warehouse inventory for Ecep Technologies <i> The English, London enterpreneur needed a .weba monitoring system that could keep track of products, purchases,
+deliveries and sales activities of the company. The tool that was used was Laravel 5.1 MVC</i>
+<br/><br/>
+
+<strong> In 2016</strong> Contracted as freelance, standalone Full stack Developer for American project Noblelinx CRM upgrades and extensions
+<br/><br/>
+
+<strong> In 2015</strong> Contracted by Interactive Data as freelance Front-end Developer for its new intranet project "Infobroker" hosted by Commerbank Germany.
+<br/><br/>
+
+<strong> In 2014</strong> Short passings from "DeltaCosmos Security" and "CCS Education" as Full stack and Javascript Develper respectively
+<br/><br/>
+
+<strong> In 2013</strong> Self-occupied as freelance "Web Developer and Graphics Designer"
+<br/><br/>
+
+<strong> In 2012</strong> Graduated College with a 'first class honors' degree <i>and attached a 'student of the year' distinction for last year performance.</i>
+<br/><br/>
+
+<strong> In 2011 </strong> I was released from Sanofi aventis after 11 years employment.<i>The recession that stroke the country has led company to unleash almost half of the crew in
+near three years time.</i>
+<br/><br/>
+
+<strong> In 2009</strong> <i>I have decided to pursue a bachelor diploma on the domain of web technology. Nevertheless, while still occupied by 'sanofi aventis', has modified
+my decision in a way to select a title that would add value on the company as well. With those in mind </i> Enrolled 'Internet Engineering and Web Management' class from
+University of Greenwich associated with 'New York' College.
+<br/><br/>
+
+<strong> In 2008</strong> Enrolled full-year class at Athenian Vocational Institute ‘Integraphics’ labeled ‘Web Designed & Development’. <i>During the class, I faced a
+well structured introduction to Web protocols like HMLT, CSS, javascript and an infantile meeting experience with PHP, MySQL and Apache. While the training turned out
+really advantageous and the outcome was my first web site, build with ajax technology, exclusively consisting on protocols, my appetite on comprehending web technologies
+was enormous and the need to integrate myself with a root understanding over the concepts has generated my interest to obtain more advanced education.</i>
+<br/><br/>
+
+
+<strong> In 2006</strong> Graduated 'Graphics designer for printable mediums & Web' at 'AKMI' Vocational Institute<i>, and up until 2008 I have urged as one
+and primary target to familiarize and conform myself with the new business environment that had arose at ‘Sanofi Aventis’. Starting from the same year, up until 2008 I was
+gradually assigned to provide office assistance for the 'Facility and HSE Department' by recording, maintaining and draw out monthly reports for the data housekeeping.</i>
+<br/><br/>
+
+<strong> In 2005</strong>, <i>while feeling the need to integrate a 'Graphic Designer' certificate that would ground further my qualifications in the area,</i> I have enrolled
+a full-year class at 'AKMI' Vocational Institute labeled 'Graphics Designer for Printable Mediums and Web'. <i>There I had my original experience with web concepts, namely
+site design and development, since we gained practicing skills on Macromedia's web tools such as Dreamweaver & Flash.
+At the same time in 2005 , while still occupied by 'Aventis Pharma' Pharmaceutical, there was a 'sudden merger announced by our company with 'Sanofi' Pharmaceutical.
+The merger was of a great value since the new-constitute company would climb high above previous competitive levels, gaining huge market shares would now strive c
+ompeting top ranking pharmaceuticals world-wide. </i>
+<br/><br/>
+
+<strong> In 2002</strong> Graduated 3D-Animation class at 'AKMI' vocational institute <i> and for the later 3 years I primarily focused on my job, while as a hobby I continued to practice my
+skills with graphics design and 3D Animation components, as Adobe Photoshop, Corel Draw and 3D-Max respectively.
+</i>
+<br/><br/>
+
+
+<strong> In 2001</strong> Employed by ‘aventis pharma’ world-wide pharmaceutical enterprise <i>, maintaining headquarters to France and establised subsidiaries word-wide.</i>
+<br/><br/>
+
+<strong> In 2000</strong> Enrolled 2 years class, ‘3D Animation & Desktop Publishing’ at ‘AKMI’ Athenian private vocational institute.
+<i>For the first year we have been practicing strictly working 'on-hand' as to 'Free Hand Drawing', 'Paper Drawing Animations' and issuing 'Page Models' for magazines etc,
+while also studying 'Art History' and 'Typography History'. The next year I had my first experience with computing using tools like 'Photoshop 5.5', 'Corel Draw 8' and '3D-Max 3'</i>
+<br/><br/>
+
+</p>
+</div>
+</div>
+<div id="interactive_cv_field">
+    <h2>Interactive Timeline</h2>
+<ul id='timeline_btns'>
+    <li><a id="mlt" href="#">Clear</a></li>
+<li><a id="std" href="#">Studies</a></li>
+<li><a id="job" href="#">Work</a></li>
+</ul>
+<ul id="objectives">
+    <li id="label">Label</li>
+    <li id="skills">Skills</li>
+    </ul>
+    <ul id="timeline">
+    <li class="default">1999</li>
+    <li class="std_start_off">
+    <a href="#" title="Enroll 'Desktop Publising and 3D Animation' | ">2000</a>
+    <p title="item_label">Enroll 'Desktop Publishing and 3D Animation' | AKMI Vocational Institute</p>
+<p title="item_skills">
+    For the first year we have been practicing strictly working 'on-hand' as to 'Free Hand Drawing', 'Paper Drawing Animations' and issuing 'Page Models' for magazines etc, while also studying 'Art History' and
+'Typography
+History'. The next year I had my first experience with computing using tools like 'Photoshop 5.5', 'Corel Draw 8' and '3D-Max 3'
+</p>
+</li>
+<li class="job_start_off">
+    <a href="#" title="Employed in 'sanofi aventis' ">2001</a>
+    <p title="item_label">Employed by 'Aventis Pharma'</p>
+<p title="item_skills">
+    I was employed as 'messenger' of the company with the 'out world' partners, while also providing random assistance, whenever it was required, within the premises. At the moment ‘aventis pharma’ was a newly
+consisted
+company, the outcome merger of 'rhone pouleng rhorer' and 'hegst marrion russel' pharmaceuticals, that its competitiveness was ranking more or less at the middle.
+</p>
+</li>
+<li class="std_end_off"><a href="#" title="Graduate 'Desktop Publising and 3D Animation' ">2002</a>
+    <p title="item_label">Graduate 'Desktop Publishing and 3D Animation' | AKMI Vocational Institute</p>
+<p title="item_skills">-</p>
+    </li>
+    <li class="default">2003</li>
+    <li class="default">2004</li>
+    <li class="std_start_off">
+    <a href="#" title="Enroll 'Graphics Design for Printable Mediums and Web' ">2005</a>
+    <p title="item_label">Enroll 'Graphics Design for Printable Mediums and Web' | AKMI Vocational Institute</p>
+<p title="item_skills">There I had my original experience with web concepts, namely site design and development, since we gained practicing skills on Macromedia's web tools such as Dreamweaver & Flash. </p>
+</li>
+<li class="std_end_off"><a href="#" title="Graduate 'Graphics Design for Printable Mediums and Web' ">2006</a>
+    <p title="item_label">Graduate 'Graphics Design for Printable Mediums and Web' | AKMI Vocational Institute</p>
+<p title="item_skills">-</p>
+    </li>
+    <li class="job_start_off">
+    <a href="#" title="'Aventis Pharma' merged with 'Sanofi' Pharmaceutical">2007</a>
+    <p title="item_label">'Aventis Pharma' merged with 'Sanofi' Pharmaceutical</p>
+<p title="item_skills">
+    At the same time in 2005 , while still occupied by 'Aventis Pharma' Pharmaceutical, there was a 'before the gate' major merger announced by our company with 'Sanofi' Pharmaceutical. The merger is purposely
+described as
+'major' since the newborn company would climb high above previous competitive placements, and gaining huge market shares would now strive in competition to top ranking pharmaceuticals world-wide.
+</p>
+</li>
+<li class="std_start_off">
+    <a href="#" title="Enroll 'Web Design & Development' ">2008</a>
+    <p title="item_label">Intergraphics | Enroll 'Web Design and Development'</p>
+<p title="item_skills"> During the class, I faced a well structured introduction to Web protocols like HMLT, CSS, javascript and an infantile meeting experience with PHP, MySQL and Apache. While the training turned out
+really advantageous and the outcome was my first web site, build with ajax technology, exclusively consisting on protocols, my appetite on comprehending web technologies was enormous and the need to integrate
+myself
+with a root understanding over the concepts has generated my interest to obtain more advanced education.</p>
+</li>
+<li class="std_start_off"><a href="#" title="Graduate 'Web Design & Development | Enroll 'Internet Engineering & Web Management' ">2009</a>
+    <p title="item_label">New York College in Association to University of Greenwich | Internet Engineering and Web Management</p>
+<p title="item_skills">
+
+    </p>
+    </li>
+    <li class="default">2010</li>
+    <li class="job_start_off"><a href="#" title="Released from Sanofi-Aventis Pharmaceutical">2011</a>
+    <p title="item_label">Released from Sanofi-Aventis Pharmaceutical</p>
+<p title="item_skills">
+    I was released from Sanofi aventis after 11 years employment.The recession that stroke the country has led company to unleash almost half of the crew in near three years time;
+</p>
+</li>
+<li class="std_end_off"><a href="#" title="Graduate 'Internet Engineering & Web Management' ">2012</a>
+    <p title="item_label">Graduate 'Internet Engineer and Web Management' | University of Greenwich</p>
+<p title="item_skills">
+    Graduated College with a 'first class honors' degree from University and attached a 'student of the year' distinction for last year performance. My final Thesis was proposed for adoption by the associated 'New York
+College' for Maintaining and Scheduling institute's year resources.
+</p>
+</li>
+<li class="job_start_off"><a href="#" title="Self-employed as 'Web Designer and Developer' ">2013</a>
+    <p title="item_label">Self-Employeed 'Web Design and Development'</p>
+<p title="item_skills">
+    Take over freelance projects for local merchants. Mainly invoked hardcoded storefronts and web solutions completely designed and developed from scratch, grounded on web technologies like PHP, JS, XSLT, HTML5, CSS3
+more.
+    Most prominent project, ELGREG, a mixture of all afordmentioned techs provided a hardcoded bottom-up storefront and backend portal.
+</p>
+</li>
+<li class="job_start_off"><a href="#" title="Short Passings from 'Deltacosmos' and 'CCS Education'' ">2014</a>
+    <p title="item_label">Employeed shortly for 'Deltacosmos' and 'CCS Education'</p>
+<p title="item_skills">Occupied as Full stack and Front-end Developer for the companies corespondingly, maintaining, upgrading and extending 'Deltacosmos' ERP and building a JSON API for 'CSS Eduction' that would be
+consumed
+internally by HMTL5 - Javascript application
+</p>
+</li>
+<li class="job_start_off"><a href="#" title="Contracted to 'Interactive Data' as Front-end Developer for German 'Commerzbank' intranet">2015</a>
+    <p title="item_label">Contracted 'Remote Web Developer' for German 'Commerzbank Infobroker'</p>
+<p title="item_skills">
+    Occupied as a part of Scrum maintained, multicultural developers environment for the implementation of Commerzbank Intranet project, namely Infobroker. Main responsibility was to collect, design
+and retrieve proprietary data - or complete data-views - through Interactive Data web services, using a custom graphical interface of enterprise. The final output would be visually styled for web monitoring, dictated
+strictly through customer specifications.
+</p>
+</li>
+<li class="job_start_off"><a href="#" title="Contracted remotely as Full Stack Developer to 'Seecago' for American Noblelinx CRM">2016</a>
+    <p title="item_label">Contracted 'Remote Full Stack Developer' for American 'Noblelinx CRM'</p>
+<p title="item_skills">
+    Occupied remotely as standalone Full Stack Developer in Agile mode with the Manager, on a lecacy CRM system, originated nearly a decade backwards. Key assingment was to maintain and upgrade legacy coded mixed
+technologies,
+    to more efficient, controllable environment and to design and adjust new features that would add value to company's daily routines.
+</p>
+</li>
+</ul>
+</div>
+<div id="introduction_cv_field">
+    <div class="main_txt">
+    <h2>Welcome</h2>
+    <p>
+    <span>Hi! My name is Manos.</span>
+For near the last and a half decade I have been studying and practicing my skills on diverse domains of educational interest,
+    attending a bachelor degree from an English University while also taking multiple full-year trainings on Athenian private vocational institutes.
+    The aspects of my study and research could be, in an abstruct manner, summarized to 'Web and Desktop Applications Development','Business Management and Marketing' and 'Graphics Design'.
+    At the same time, speaking of my occupational experience, for more than a decade I have been occupied and have been evolving my competences and communication in a top-competent,world-wide extending
+pharmaceutical company, complying and assimilating to my profile highly strict and demanding business procedures.
+</p>
+</div>
+<div class="aux_txt">
+    <h3>What is the site about</h3>
+<p>
+On site you may resume both of my occupational and educational timelines, how the two were drawn and evolve in parellel during previous years. Context may be indexed in detail,
+either as bulleted descriptions or guided by an interactive timeline. Along with the descriptive context you may also have an overview on some of the most critical projects that I have encountered.
+    Therefore you may find available work-samples while working as a freelance developer on the Web, as officer occupied by pharmaceutical company, and last but not least as student at university.
+    For the latter you may additionally index the scores attained, instructors’ comments, while also documentation and video reference whenever appropriate.
+    Finally on the last page there are available links to the officials sites for each cited organization. You are suggested to request for reference where available. If you wish you may contact me
+using any of my personal accounts provided for some of the most popular social networks. Please suggest the context if you feel like.
+</p>
+</div>
+</div>
+</div>
+</div>
+        
+        `
+
+    )
+}
