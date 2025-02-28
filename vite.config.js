e77179b262517b4480246f4e6d5ebc3e1924a13b
manos-zalokostas <manos.zalokostas@gmail.com>
2025-02-28 17:13:53 +0200

    #LIT3-LESS | COMMIT 'GITHUB' REQUIRED FILES ======================================================  - COMMIT FILES

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import string from 'vite-plugin-string'; // This handles importing SCSS files as strings
 
 export default defineConfig({
+    base: process.env.VITE_BASE_URL || "/",
     server: {
         open: true,  // Automatically open browser on server start
         host: true,  // Allow external access for testing
@@ -9,6 +10,7 @@ export default defineConfig({
     },
     build: {
         target: 'esnext',  // Use modern ES syntax for the build
+        outDir: 'dist',
     },
     plugins: [
         string({
