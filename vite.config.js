57a9195e22d1452d2f6cab7bfd085b7ed810e734
manos-zalokostas <manos.zalokostas@gmail.com>
2025-02-28 23:14:02 +0200

    #LIT3-LESS | 'VITE SERVER' PROPERTY ====================================================== - ADD PRODUCTION HOST 'APPJOI.COM' TO VITE 'ALLOWED-HOSTS'

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import string from 'vite-plugin-string'; // This handles importing SCSS files as strings
 
 export default defineConfig({
-    base: process.env.VITE_BASE_URL || "/",
+    base: "/micv",
     server: {
         open: true,  // Automatically open browser on server start
         host: true,  // Allow external access for testing
         port: 3000,  // Set the dev server port
+        allowedHosts: ["appjoi.com"]
     },
     build: {
         target: 'esnext',  // Use modern ES syntax for the build
